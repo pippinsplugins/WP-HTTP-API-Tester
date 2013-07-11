61752eae1cf6da2a734da75b282df0fb06a480da
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-11 14:20:58 -0500

    Adding first file

---
+++ wp-http-api-tester.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: WP HTTP API Tester
+ * Description: Provides an interface for testing remote requests via the WP HTTP API
+ * Author: Pippin Williamson
+ * Contributors: mordauk
+ * Version: 0.1
+ */
