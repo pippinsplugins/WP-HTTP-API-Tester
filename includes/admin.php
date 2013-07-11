a5ac224559bdc78eba3dbf80d8bf9d7b66f0ce10
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-11 14:56:57 -0500

    Add blank admin page

--- includes/admin.php
+++ includes/admin.php
@@ -1,5 +1,28 @@
 <?php
 
 class WP_HTTP_API_Tester_Admin {
-	
+
+	public function __construct() {
+		add_management_page(
+			__( 'HTTP API Tester', 'wp-http-api-tester' ),
+			__( 'HTTP API Tester', 'wp-http-api-tester' ),
+			'manage_options',
+			'wp-http-api-tester',
+			array(
+				$this,
+				'admin_page'
+			)
+		);
+	}
+
+	public function admin_page() {
+		?>
+		<div class="wrap">
+			<?php screen_icon( 'tools' ); ?>
+			<h2><?php _e( 'HTTP API Tester', 'wp-http-api-tester' ); ?></h2>
+		</div>
+		<?php
+	}
+
 }
+new WP_HTTP_API_Tester_Admin;
